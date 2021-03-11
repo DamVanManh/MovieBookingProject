24a61869985578237b6efebd13b8b260056fb3c1
DamVanManh <hnammad7@gmail.com>
2021-03-12 00:29:10 +0700

    gom hình vào thư mục carousel

--- src/components/Carousel/popup/index.js
+++ src/components/Carousel/popup/index.js
@@ -8,7 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import useStyles from "./styles";
 import './css.css'
-const play = './img/play-video.png';
+const play = './img/carousel/play-video.png';
 
 export default function PopupTrailer(props) {
   const [open, setOpen] = React.useState(false);
