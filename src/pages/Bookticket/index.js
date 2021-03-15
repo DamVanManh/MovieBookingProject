bda09954d6cdeb7190e213285058683a26d9c5de
Phu <phuhuynhnguyen98@gmail.com>
2021-03-15 16:22:38 +0700

    additional functions for seat reservation and calculate total money

--- src/pages/Bookticket/index.js
+++ src/pages/Bookticket/index.js
@@ -1,20 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { getRoomTicketList } from '../../reducers/actions/Movie';
 import { useParams } from "react-router-dom";
 
 export default function Bookticket() {
+  // mảng chứa các ghế được chọn
+  const [seatReserved, setSeatReserved] = useState([])
+  // console.log(seatReserved)
 
+  const [total, setTotal] = useState(0)
+
+  // LẤY MÃ LỊCH CHIẾU DƯỚI MATCH VÀ GỌI API
   const param = useParams()
-  // console.log(param)
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getRoomTicketList(param.maLichChieu))
   }, [param.maLichChieu])
 
 
+  // LẤY DATA TỪ STORE VỀ
   const { seatList, loading, error } = useSelector((state) => state.movieReducer);
-  // console.log(loading)
   if (loading) {
     return <h1>loading</h1>
   }
@@ -22,15 +27,45 @@ export default function Bookticket() {
     return <div>{error}</div>
   }
 
-  // console.log(seatList.danhSachGhe)
+  // ĐẶT VÉ VÀ THÀNH TIỀN
+  const handleSelect = (seat) => {
+    if (seatReserved.indexOf(seat) > -1) {
+      setTotal(total - seat.giaVe)       // trừ đi tiền của ghế vừa bỏ
+      setSeatReserved(seatReserved?.filter(res => res !== seat))    // xóa khỏi res
+    } else {
+      setSeatReserved(pre => [...pre, seat])      // thêm vào res
+      setTotal(total + seat.giaVe)      // cộng thêm tiền của ghế vừa đặt
+    }
+  }
+
+
   return (
     <div>
       <div className="container">
         <div className="row">
+          <div className="col-sm-9">
+            <p>Ghế đã chọn:</p>
+            <ul>
+              {seatReserved.map((item, index) => {
+                return (
+                  <li key={index}>Ghế số:{item.tenGhe}</li>
+                )
+              })}
+            </ul>
+          </div>
+          <div className="col-sm-3">
+            <h1>Thành tiền:</h1>
+            <h1>{total}đ</h1>
+
+
+          </div>
+        </div>
+        <div className="row">
           {seatList.danhSachGhe?.map((seat, index) => {
             return (
-              <div className="col-sm-2 pb-2 ml-2">
-                <button className="btn btn-success">Ghế {seat.tenGhe}</button>
+              <div key={index} className="col-sm-2 pb-2 ml-2">
+
+                <button className={`btn ${seat.daDat ? 'btn-danger' : `${seatReserved.indexOf(seat) > -1 ? 'btn-warning' : 'btn-success'} `} `} disabled={seat.daDat} onClick={() => handleSelect(seat)} >Ghế {seat.tenGhe}</button>
               </div>
             )
           })}
