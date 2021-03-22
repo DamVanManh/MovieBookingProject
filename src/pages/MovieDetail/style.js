aa994c4e576eee4c9fdcafb9a96d1007afa9218b
Phu <phuhuynhnguyen98@gmail.com>
2021-03-22 16:32:39 +0700

    update css for movieDetail

--- src/pages/MovieDetail/style.js
+++ src/pages/MovieDetail/style.js
@@ -1,36 +1,77 @@
 import { makeStyles } from "@material-ui/core"
 
-const useStyle = makeStyles({
+const useStyle = makeStyles((theme) => ({
 
-    movie__detail: {
-        // backgroundImage: 'url("../img/mobile/backapp.jpg")',
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+
+    ul: {
+        listStyle: 'none'
+    },
+    movieDetail: {
+        // background: 'linear-gradient(to top, rgb(10, 32, 41), transparent 100%)',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
-        // color: 'white',
-        height: 650,
+        height: '600px',
         display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center'
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+        color: 'white',
     },
-
-    movie__img: {
-        width: '30%',
-        height: '400px',
+    blur: {
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        background: 'linear-gradient(to top, rgb(10, 32, 41), transparent 100%)',
     },
-
-    movie__name: {
-        textAlign: 'center',
+    movieImg: {
+        paddingLeft: '70px',
+        paddingRight: '20px',
+        '& img': {
+            height: '350px',
+            borderRadius: '10px'
+        },
     },
-
-    movie__rating: {
-        width: '200px',
-        height: '200px',
-        lineHeight: '200px',
-        textAlign: "center",
-        border: '2px solid black',
+    movieRating: {
+        width: '120px',
+        height: '120px',
+        border: '9px solid gray',
         borderRadius: '50%',
+        lineHeight: '108px',
+        textAlign: 'center',
+        margin: '0 auto',
+        fontWeight: 'bold',
+        fontSize: '60px'
+    },
 
+    nameTheater: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        '& img': {
+            width: '50px',
+            height: '50px',
+            marginRight: '20px'
+        }
+    },
+
+    movieInfo: {
+        color: 'white',
+        fontWeight: 'bold',
+        width: '50%',
+        margin: '0 auto',
+        justifyContent: 'space-around',
+        marginBottom: '45px'
     }
 
-})
+}))
 export default useStyle
