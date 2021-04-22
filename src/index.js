65da8891d6c560c9d32c8d1d6ec0d9d41bbe1b29
DamVanManh <hnammad7@gmail.com>
2021-04-22 21:48:03 +0700

    fix redux-devtools

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './styles/normalize.css';
 
 const enhanced = compose(
   applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
 );
 const store = createStore(rootReducer, enhanced);
 
