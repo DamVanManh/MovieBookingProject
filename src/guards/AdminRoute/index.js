0c399e673d5bcacae9de3d77477ac07be36456ea
DamVanManh <hnammad7@gmail.com>
2021-06-22 15:26:25 +0700

    check admin mobile

--- src/guards/AdminRoute/index.js
+++ src/guards/AdminRoute/index.js
@@ -5,12 +5,12 @@ import { useSelector } from "react-redux";
 
 function AdminRoute(props) {
   const { currentUser } = useSelector((state) => state.authReducer);
-  const { component: UsersManagement, ...rest } = props;
+  const { component: ComponentAdmin, ...rest } = props;
   return (
     <Route {...rest} render={(routeProps) => {
       if (currentUser) {
         if (currentUser.maLoaiNguoiDung === 'QuanTri') {
-          return <UsersManagement {...routeProps} />
+          return <ComponentAdmin {...routeProps} />
         }
         return <Redirect to='/' />
       }
